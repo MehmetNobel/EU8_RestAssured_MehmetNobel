ec7f7d99e985839c5df5e8f9d403154b156cbe24
Mehmet <mehmetnobel26@gmail.com>
2022-07-12 00:16:25 +0300

    Initial commit

---
+++ src/test/java/com/cydeo/day1/SimpleGetRequest.java
@@ -0,0 +1,29 @@
+package com.cydeo.day1;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class SimpleGetRequest {
+
+    String url="http://35.171.2.97:8000/api/spartans";
+
+    @Test
+    public void test1(){
+
+        //sending request
+        Response response = RestAssured.get(url);
+
+        //writing the response type
+        System.out.println("response.statusCode() = " + response.statusCode());
+
+        //printing the message.
+        response.prettyPrint();
+
+
+    }
+
+
+
+
+}
