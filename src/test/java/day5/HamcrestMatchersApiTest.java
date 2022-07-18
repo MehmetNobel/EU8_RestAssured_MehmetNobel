e883caf92009a1617a62373047c571443c7eb5a2
Mehmet <mehmetnobel26@gmail.com>
2022-07-18 13:23:18 +0300

    HamcrestMatchersApiTest

---
+++ src/test/java/day5/HamcrestMatchersApiTest.java
@@ -0,0 +1,25 @@
+package day5;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class HamcrestMatchersApiTest {
+
+    @DisplayName("one spartan with Hamcrest and chaining")
+    @Test
+    public void test1(){
+
+
+
+
+    }
+
+
+
+
+
+}
