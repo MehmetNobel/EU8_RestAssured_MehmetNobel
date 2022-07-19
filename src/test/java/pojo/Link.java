d06205b0d3cf82dc0c643d0016afdde7837c25f0
Mehmet <mehmetnobel26@gmail.com>
2022-07-19 23:18:48 +0300

    POJO CLASSESS ADDED - 1.way

--- src/test/java/pojo/Link.java
+++ src/test/java/pojo/Link.java
@@ -1,32 +1,19 @@
 package pojo;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+//automatically brings getters setters and toString methods.
+
 public class Link {
 
     private String rel;
     private String href;
 
 
-    public String getRel() {
-        return rel;
-    }
 
-    public void setRel(String rel) {
-        this.rel = rel;
-    }
-
-    public String getHref() {
-        return href;
-    }
-
-    public void setHref(String href) {
-        this.href = href;
-    }
-
-    @Override
-    public String toString() {
-        return "Link{" +
-                "rel='" + rel + '\'' +
-                ", href='" + href + '\'' +
-                '}';
-    }
 }
