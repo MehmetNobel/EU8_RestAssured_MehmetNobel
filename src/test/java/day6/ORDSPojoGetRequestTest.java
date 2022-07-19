1d265f8ec37a6d17d4f0a8efac87eee75efd37ab
Mehmet <mehmetnobel26@gmail.com>
2022-07-19 19:09:05 +0300

    POJO CLASSESS ADDED - 1.way

--- src/test/java/day6/ORDSPojoGetRequestTest.java
+++ src/test/java/day6/ORDSPojoGetRequestTest.java
@@ -1,6 +1,9 @@
 package day6;
 
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
+import pojo.Region;
 import utilities.HRTestBase;
 
 import java.util.List;
@@ -17,7 +20,21 @@ public class ORDSPojoGetRequestTest extends HRTestBase {
     @Test
     public void regionTest(){
 
+        JsonPath jsonPath = given().accept(ContentType.JSON).get("/regions").then()
+                .statusCode(200).extract().jsonPath();
 
+        //we are converting the first element to the region class.
+        Region regionObject = jsonPath.getObject("items[0]", Region.class);
+
+        System.out.println("regionObject = " + regionObject);
+
+        System.out.println("regionObject.getRegion_id() = " + regionObject.getRegion_id());
+
+        System.out.println("regionObject.getRegion_name() = " + regionObject.getRegion_name());
+
+        System.out.println("regionObject.getLinks().get(0) = " + regionObject.getLinks().get(0));
+
+        System.out.println("regionObject.getLinks().get(0).getHref() = " + regionObject.getLinks().get(0).getHref());
 
 
     }
