4dbbafd60ef6183bd68af947122ca491673f84b7
Mehmet <mehmetnobel26@gmail.com>
2022-07-23 20:38:44 +0300

    POST REQUEST ADDED

--- src/test/java/utilities/SpartanAuthTestBase.java
+++ src/test/java/utilities/SpartanAuthTestBase.java
@@ -5,18 +5,13 @@ import org.junit.jupiter.api.BeforeAll;
 
 import static io.restassured.RestAssured.baseURI;
 
-public class SpartanAuthTestBase {
+public abstract class SpartanAuthTestBase {
 
     //BeforeAll is a annotation equals to @BeforeClass in testNg, we use with static method name.
     @BeforeAll
     public static void init() {
         baseURI = ConfigurationReader.getProperty("spartanAuthUrl");
 
-        String dbUrl = ConfigurationReader.getProperty("spartanDataBase");
-        String dbUsername = ConfigurationReader.getProperty("spartanUserName");
-        String dbPassword = ConfigurationReader.getProperty("spartanPassword");
-
-        //DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
 
     }
 
