6e092c723af0e1d4bdb009e64c65ed6567b43b2a
Mehmet <mehmetnobel26@gmail.com>
2022-07-24 13:50:44 +0300

    POST REQUEST ADDED

--- src/test/java/day8/SpartanWithAuthTests.java
+++ src/test/java/day8/SpartanWithAuthTests.java
@@ -39,13 +39,19 @@ public class SpartanWithAuthTests extends SpartanAuthTestBase {
                 .log().all();
 
     }
-
-
-
-
-
-
-
+@DisplayName("DELETE /api/spartans{id} as editor user expect 403")
+    @Test
+    public void testEditorDelete() {
+        given()
+                .auth().basic("editor", "editor")
+                .and().accept(ContentType.JSON)
+                .and().pathParam("id", 30)
+       .when()
+                .delete("/api/spartans/{id}")
+       .then()
+                .statusCode(403)
+                .log().body();
+    }
 
 
 
